af8b4f4903da355074fe27e1ed382b833604560f
Abdurakhmon Jamoliddinov <jamoliddinovabdu@gmail.com>
2023-04-05 13:08:16 +0500

    Initial Commit

---
+++ MyFinance-I/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyFinance-I
+//
+//  Created by Workspace (Abdurakhmon Jamoliddinov) on 05/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
